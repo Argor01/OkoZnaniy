488215b5a40fcf35cd58468bb0a802334986de14
Br1Imbrait <2005@mail.ru>
2026-03-25 18:16:34 +0700

    feat: add VK back, now 3 social icons: Telegram, VK, MAX

--- frontend-react/src/features/auth/components/SocialLoginButtons.tsx
+++ frontend-react/src/features/auth/components/SocialLoginButtons.tsx
@@ -97,6 +97,9 @@ const SocialLoginButtons: React.FC<SocialLoginButtonsProps> = () => {
         <a href="#" onClick={handleTelegramAuth} aria-label="Telegram">
           <img src="/assets/telegram.png" alt="telegram-login" className="socialLoginIcon" />
         </a>
+        <a href={vkHref} aria-label="VK">
+          <img src="/assets/vk.png" alt="vk-login" className="socialLoginIcon" />
+        </a>
         <a href={vkHref} aria-label="MAX">
           <img src="/assets/max.svg" alt="max-login" className="socialLoginIcon" />
         </a>
