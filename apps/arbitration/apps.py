d1e9dc7785d60317c5c02422b433d1f2607a49d3
Br1Imbrait <2005@mail.ru>
2026-03-30 12:57:28 +0700

    fix: Добавлен apps.py для приложения arbitration

--- apps/arbitration/apps.py
+++ apps/arbitration/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class ArbitrationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'arbitration'
+    name = 'apps.arbitration'
+    verbose_name = 'Арбитраж'
